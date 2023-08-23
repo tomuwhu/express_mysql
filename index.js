88173c302462748edb479c7913423f712d535add
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-23 21:32:32 +0200

    .

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const app = express()
 const port = 3000
 const mysql = require('mysql')
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "test"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
 });
 
 app.get('/', (req, res) => {
-    con.connect(() => {
-        con.query("select * from user", (err, data) => {
-            res.send(data)
-        })
+  con.connect(() => {
+    con.query("select * from user", (err, data) => {
+      res.send(data)
     })
+  })
 })
 
 app.get('/:un', (req, res) => {
-    con.connect(() => {
-        con.query(`select * from user where un = '${req.params.un}'`, (err, data) => {
-            res.send(data)
-        })
+  con.connect(() => {
+    con.query(`select * from user where un = '${req.params.un}'`, (err, data) => {
+      res.send(data)
     })
+  })
 })
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-    console.log(`http://localhost:${port}/tom`)
+  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${port}/tom`)
 })
